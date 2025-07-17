da19447a09546681af97e671b6141775f41b7235
Alexander <58055542+IMrProYTI@users.noreply.github.com>
2025-07-17 22:59:15 +0300

    Add vue-router for UI framework

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,19 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 import { createPinia } from 'pinia'
+import ui from '@nuxt/ui/vue-plugin'
+import { createRouter, createWebHistory } from 'vue-router'
 
 const pinia = createPinia();
-const app = createApp(App)
+const app = createApp(App);
+
+const router = createRouter({
+  routes: [],
+  history: createWebHistory()
+})
 
 app.use(pinia);
+app.use(router);
+app.use(ui);
+
 app.mount('#app')
