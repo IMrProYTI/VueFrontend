c321c331cdbbac9b80af013e912da42032e9bd46
Alexander <58055542+IMrProYTI@users.noreply.github.com>
2025-07-17 23:56:32 +0300

    Base styles and html

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,47 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
   <UApp>
-    <div>
-      <a href="https://vite.dev" target="_blank">
-        <img src="/vite.svg" class="logo" alt="Vite logo" />
-      </a>
-      <a href="https://vuejs.org/" target="_blank">
-        <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-      </a>
+    <div class="container m-auto space-y-4 my-8">
+      <div class="flex space-x-2">
+        <h1 class="w-fit">Учётные записи</h1>
+        <UButton icon="i-heroicons:plus" />
+      </div>
+      <InfoPanel>Для указания нескольких меток для одной пары логин/пароль используйте разделитель ;</InfoPanel>
+      <UTable :columns="columns" :data="data" class="flex-1" />
     </div>
-    <HelloWorld msg="Vite + Vue" />
   </UApp>
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
+<script setup lang="ts">
+import { ref } from 'vue';
+import type { TableColumn } from '@nuxt/ui';
+
+
+interface IAccounts {
+  label: string
+  type: 'LOCAL' | 'LDAP'
+  login: string
+  password: string | null
 }
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+
+
+const columns: TableColumn<IAccounts>[] = [
+  {
+    accessorKey: 'label',
+    header: "Метка"
+  },
+  {
+    accessorKey: 'type',
+    header: "Тип записи"
+  },
+  {
+    accessorKey: 'login',
+    header: "Логин"
+  },
+  {
+    accessorKey: 'password',
+    header: "Пароль"
+  }
+];
+
+const data = ref<IAccounts[]>([]);
+</script>
---
+++ src/components/InfoPanel.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="flex items-center rounded-lg p-3 space-x-2 text-black bg-blue-400">
+		<UIcon class="size-6" name="i-heroicons:question-mark-circle" />
+		<p>
+			<slot />
+		</p>
+	</div>	
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
